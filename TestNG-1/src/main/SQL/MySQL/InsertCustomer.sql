4c8f23362cbfd327797fe6f334bea8fff2952b42
Ralic Lo <raliclo@gmail.com>
2016-08-30 21:55:42 -0700

    Relocate SQL folder

---
+++ TestNG-1/src/main/SQL/MySQL/InsertCustomer.sql
@@ -0,0 +1,8 @@
+INSERT INTO spring.Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+
+INSERT INTO spring.Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Illegal', 'Jim Yang', '245 Market Street', 'Mountain View', '23106', 'USA');
+
+INSERT INTO spring.Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Tasty Pot', 'Ralic Lo', '567 Street', 'Taoyuan', '401236', 'Taiwan');
