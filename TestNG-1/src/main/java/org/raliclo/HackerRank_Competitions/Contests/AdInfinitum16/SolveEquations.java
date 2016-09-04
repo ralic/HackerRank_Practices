bb73c785a0b6c9853b5a41ce25579a59dffaf186
ralic <raliclo@gmail.com>
2016-09-03 19:21:21 -0700

    PrimeFactors, SolveEquations

---
+++ TestNG-1/src/main/java/org/raliclo/HackerRank_Competitions/Contests/AdInfinitum16/SolveEquations.java
@@ -0,0 +1,99 @@
+package org.raliclo.HackerRank_Competitions.Contests.AdInfinitum16;/**
+ * Created by raliclo on 9/3/16.
+ * Project Name : TestNG-1
+ */
+//https://www.hackerrank.com/contests/infinitum16-firsttimer/challenges/solve-equations
+/* Input
+
+ax+by=c
+1x + 2000y= 999
+
+IN
+9
+1 2000 999
+1 100 21
+2 3 1
+2 5 19
+2 5 3
+2 20 8
+1000000 100000000 100000000
+2134     1234567    98765432
+77777 999998 100000000000000
+
+OUT
+999 0
+21 0
+2 -1
+2 3
+4 -1
+4 0
+100 0
+307774 -452
+142214 99989139
+*/
+
+// TODO Partially Solved
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class SolveEquations {
+    static int precision = 10;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long q = in.nextLong();
+        Point ans;
+        while (q-- > 0) {
+            BigDecimal a = BigDecimal.valueOf(in.nextLong()).setScale(0);
+            BigDecimal b = BigDecimal.valueOf(in.nextLong()).setScale(0);
+            BigDecimal c = BigDecimal.valueOf(in.nextLong()).setScale(0);
+            ans = solver(a, b, c);
+            System.out.println(ans.x + " " + ans.y);
+        }
+    }
+
+    static Point solver(BigDecimal a, BigDecimal b, BigDecimal c) {
+        BigDecimal x = BigDecimal.ONE;
+        BigDecimal y;
+        BigDecimal dist;
+//        BigDecimal z;
+//        // a*x+b*y=c
+//        if (c.compareTo(b)==1) {
+//            y=c.divide(b,BigDecimal.ROUND_DOWN);
+//            z=c.subtract(b.multiply(y)).divide(x,BigDecimal.ROUND_DOWN).subtract(BigDecimal.ONE);
+//            if (z.compareTo(BigDecimal.ONE)==1) {
+//                x=z;
+//            }
+//        } else if (c.compareTo(b)==-1) {
+//            z=c.divide(a,BigDecimal.ROUND_DOWN).subtract(BigDecimal.ONE);
+//            if (z.compareTo(BigDecimal.ONE)==1) {
+//                x=z;
+//            }
+//        }
+        while (true) {
+            //y= (c-ax)/b
+            y = c.subtract(a.multiply(x)).divide(b, BigDecimal.ROUND_DOWN).setScale(precision, BigDecimal.ROUND_DOWN);
+//            z = c.subtract(a.multiply(x)).divide(b,1); // (c-a*x)/b
+//            System.out.println(x + " " + y );
+//            System.out.println(c.subtract(a.multiply(x)).subtract(y.multiply(b)).equals(BigDecimal.ZERO)); // (c - a * x - (int) Math.floor(y) * b)
+            // dist=c-b*y+a*x
+            dist = c.subtract(a.multiply(x)).subtract(y.setScale(0).multiply(b)).setScale(precision, BigDecimal.ROUND_DOWN);
+            ;
+            if (dist.equals(BigDecimal.ZERO.setScale(precision, BigDecimal.ROUND_DOWN))) {
+                return new Point(x, y);
+            }
+            x = x.add(BigDecimal.ONE);
+        }
+    }
+
+    static class Point {
+        BigDecimal x;
+        BigDecimal y;
+
+        Point(BigDecimal X, BigDecimal Y) {
+            this.x = X.setScale(0, BigDecimal.ROUND_DOWN);
+            this.y = Y.setScale(0, BigDecimal.ROUND_DOWN);
+        }
+    }
+}
--- TestNG-1/src/main/java/org/raliclo/HackerRank_Java/BigNumber/JavaBigDecimal.java
+++ TestNG-1/src/main/java/org/raliclo/HackerRank_Java/BigNumber/JavaBigDecimal.java
@@ -9,7 +9,33 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//https://www.hackerrank.com/challenges/java-bigdecimal?h_r=next-challenge&h_v=zen
+//https://www.hackerrank.com/challenges/java-bigdecimal
+/*
+Sample Input
+
+9
+-100
+50
+0
+56.6
+90
+0.12
+.12
+02.34
+000.000
+Sample Output
+
+90
+56.6
+50
+02.34
+0.12
+.12
+0
+000.000
+-100
+ */
+
 public class JavaBigDecimal {
 
 
