b9aa8d48d9b58533bd5ec59099e1e29ddb69fab0
ralic <raliclo@gmail.com>
2016-09-17 03:19:59 -0700

    Upgrade Hadoop to 3.0.0-alpha1

--- TestNG-1/src/main/java/org/raliclo/apache/mapreduce/SectionsByCourse.java
+++ TestNG-1/src/main/java/org/raliclo/apache/mapreduce/SectionsByCourse.java
@@ -4,9 +4,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.nio.file.Files;
@@ -31,16 +29,19 @@ public class SectionsByCourse {
         job.setCombinerClass(SectionsByCourseCombiner.class);
         job.setReducerClass(SectionsByCourseReducer.class);
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+//        FileInputFormat.addInputPath(job, new Path(args[0]));
+//        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        TextInputFormat.addInputPath(job, new Path(args[0]));
+        TextOutputFormat.setOutputPath(job, new Path(args[1]));
 
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
-
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+
     }
 
 //    public static void main(String[] args) {
