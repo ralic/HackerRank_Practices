171a353ed01f9f028d0b9b9bc774dcfc110799a8
Ralic Lo <raliclo@gmail.com>
2016-11-14 02:54:38 -0800

    Updates for Q5 , using Java.net.Files

--- TestNG-1/src/main/java/org/raliclo/apache/mapreduce_Q5/Q5Reducer.java
+++ TestNG-1/src/main/java/org/raliclo/apache/mapreduce_Q5/Q5Reducer.java
@@ -18,27 +18,38 @@ package org.raliclo.apache.mapreduce_Q5;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Q5Reducer extends
         Reducer<Text, Text, Text, Text> {
+
+    static Path hdfile;
+
     @Override
     protected void reduce(Text name, Iterable<Text> datas,
                           Context context) throws IOException, InterruptedException {
-
         /*
             Access the file system
          */
         Configuration conf = new Configuration();
+//        conf.set("fs.default.name", "hdfs://localhost:9000");
         FileSystem fs = FileSystem.get(conf);
+
+        Job.getInstance().getConfiguration()
+                .set("mapreduce.output.basename", "TEST");
 
         int count = 0;
         double sumScore = 0;
 
+        context.write(new Text("Student\tCourse\tAvgScore"), new Text());
         /*
             Write the Data Stream.
          */
@@ -52,30 +63,43 @@ public class Q5Reducer extends
             //Peter	CS557_8.333333333333334_2015_MATH
             //        0      1               2    3
             String[] inItems = item.toString().split("_");
-            String data = name.toString() + "\t" + inItems[0] + "\t" + inItems[1] + "\n";
+            String data = name.toString() + "\t" + inItems[0] + "\t" + inItems[1];
+            context.write(new Text(data.toString()), new Text());
             count++;
             sumScore += Double.parseDouble(inItems[1]);
-            org.apache.hadoop.fs.Path hdfile = (org.apache.hadoop.fs.Path) Paths.get("./src/main/java/org/raliclo/apache/mapreduce_Q5/output/" +
-                    inItems[3] + "_Dept.txt");
-//            Path netfile = Paths.get("./src/main/java/org/raliclo/apache/mapreduce_Q5/output/" +
-//                    inItems[3] + "_Dept.txt");
 
+            hdfile = new Path("./src/main/java/org/raliclo/apache/mapreduce_Q5/output/" +
+                    inItems[3] + "_Dept.txt");
             FSDataOutputStream fsOutStream;
+
+            byte[] dataBytes = (data.toString() + "\n").getBytes();
             if (!fs.exists(hdfile)) {
                 fsOutStream = fs.create(hdfile);
-                fsOutStream.writeUTF("Student\tCourse\tAvgScore\n");
-                fsOutStream.writeUTF(data);
+                Files.write(Paths.get(hdfile.toString()),
+                        "Student\tCourse\tAvgScore\n".getBytes(),
+                        StandardOpenOption.APPEND);
+                Files.write(Paths.get(hdfile.toString()),
+                        ("------\t------\t-------\n").toString().getBytes(),
+                        StandardOpenOption.APPEND);
+                Files.write(Paths.get(hdfile.toString()),
+                        dataBytes,
+                        StandardOpenOption.APPEND);
                 fsOutStream.close();
             } else {
-                fsOutStream = fs.append(hdfile);
-                byte[] dataBytes = (data.toString()).getBytes();
-                fsOutStream.write(dataBytes, 0, dataBytes.length);
-                fsOutStream.close();
-                System.out.println(fs.getFileStatus(hdfile));
+                Files.write(Paths.get(hdfile.toString()),
+                        dataBytes,
+                        StandardOpenOption.APPEND);
+
+                System.out.println("\n" + fs.getFileStatus(hdfile) + "\n");
             }
         }
-//        fsOutStream.writeUTF("------\t------\t-------\n");
-//        fsOutStream.writeUTF("Overall\t      \t" + (sumScore / count) + "\n");
+
+        Files.write(Paths.get(hdfile.toString()),
+                ("------\t------\t-------\n").toString().getBytes(),
+                StandardOpenOption.APPEND);
+        Files.write(Paths.get(hdfile.toString()),
+                ("Overall\t      \t" + (sumScore / count) + "\n\n").toString().getBytes(),
+                StandardOpenOption.APPEND);
     }
 }
 
--- TestNG-1/src/main/java/org/raliclo/apache/mapreduce_Q7/Q7Mapper.java
+++ TestNG-1/src/main/java/org/raliclo/apache/mapreduce_Q7/Q7Mapper.java
@@ -15,39 +15,44 @@
 
 package org.raliclo.apache.mapreduce_Q7;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 
 public class Q7Mapper extends
         Mapper<LongWritable, Text, Text, Text> {
 
+    int lines = 0;
+
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
 
-        String[] words = value.toString().split(",");
-        String year = words[0];
-        if (!year.equals("YEAR")) {
-            //YEAR,DEPT,COURSE,STUDENT,SCORE1,SCORE2,SCORE3,SCORE4,SCORE5
-            // 0    1     2      3      4       5      6     7     8
-            Double sum = 0.0;
-            Double avg;
-            int count = words.length - 4;
-            for (int i = 4; i < words.length; i++) {
-                sum += Integer.parseInt(words[i]);
-            }
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        org.apache.hadoop.fs.Path hdfile = (org.apache.hadoop.fs.Path)
+                Paths.get("./src/main/java/org/raliclo/apache/mapreduce_Q7/format/" +
+                        "secom_labels.data");
 
-            avg = sum / count;
-            //Text dept, Text course, Text student, Text count
-            String dept = words[1];
-            String course = words[2];
-            String student = words[3];
-            String myValue = dept + "," + course + "," + student + "," + count;
-            context.write(new Text(myValue), new Text(avg.toString()));
+        FSDataInputStream fsIntputStream;
+        if (!fs.exists(hdfile)) {
+            fsIntputStream = fs.open(hdfile);
+            System.out.println(fsIntputStream.readUTF());
+
+        } else {
+            System.out.println("File not existed : " + hdfile);
         }
+
+        lines++;
+        context.write(new Text(String.valueOf(lines)), new Text(value));
+
     }
 }
 
